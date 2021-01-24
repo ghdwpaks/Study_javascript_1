370bc59e4f5699fbd41078130f4e09ebb1b9b270
ghdwpaks <hjm7904@gmail.com>
2021-01-24 22:05:58 +0900

    2nd part of second grammar code

--- index3.js
+++ index3.js
@@ -25,11 +25,29 @@ function checkclick2() {
     }
     console.log(`title.style.color = ${title.style.color}`);
 }
-
+function checkclick3() {
+    if(title.style.color != "yellowgreen") {
+        title.style.color = "yellowgreen";
+    } else {
+        title.style.color = "cornflowerblue";
+    }
+    console.log(`title.style.color = ${title.style.color}`);
+}
 //window.addEventListener("resize",handleResize);
 window.addEventListener("resize",handleResize);
 //놀랍게도 바로 위에 줄에 handleResize는 괄호가 붙지 않는 상태로 쓴다.
 title.addEventListener("click", checkclick2);
+title.addEventListener("mouseenter", checkclick3);
+//click = 클릭하면 이벤트 인지함
+//mouseenter = 마우스를 호버링하면 인지함
+function handleoffline() {console.log("offline to server")}
+function handleonline() {console.log("online to server")}
+
+window.addEventListener("offline", handleoffline);
+window.addEventListener("online", handleonline);
+
+
+
 
 if (10 < 5) {console.log("10 < 5 = true");
 } else {console.log("10 < 5 = false");}
@@ -44,6 +62,7 @@ if(20 > 5 || "ghdwpaks" === "ghdwpaks") {console.log("or true");
 }else {console.log("false");}
 
 //질의응답을 할 수 있는 prompt라는 기능이 있지만 그냥 안쓰는게 좋다. 기본적으로 저 prompt라는 명령어를 만나게 된다면 사이트 전체가 멈추게 되며 사용자는 올바른 상호작용 값을 입력하는것에 있어서 강제성을 가지게 돼버린다.
+/*
 const age = prompt("Hello?");
 console.log(age);
 if (age > 18 && age < 150) {
@@ -56,9 +75,8 @@ if (age > 18 && age < 150) {
     console.log("You can't buy Alcohol drinks");
     alert("You can't buy Alcohol drinks");
 }
-
 const color = prompt("what's your want of text color?");
 title.style.color = color;
 console.log(`color = ${color}`);
-
+*/
 
