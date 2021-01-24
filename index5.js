64581b1b5559ef5f5f1c54f8c9426208f2af46f4
ghdwpaks <hjm7904@gmail.com>
2021-01-24 23:13:52 +0900

    3rd part of second grammar code

---
+++ index5.js
@@ -0,0 +1,28 @@
+const title = document.querySelector("#title");
+
+const title_CLASS = "clicked";
+let let_title_class = title_CLASS;
+
+function handleClick() {
+    const hasClass = title.classList.contains(title_CLASS);
+    console.log(`title.classList.contains = ${title.classList}`)
+    console.log(`hasClass = ${hasClass}`);
+    if(!hasClass) {
+        if (title.classList.contains("unclicked")) {
+            title.classList.remove(let_title_CLASS);
+        }
+        let_title_CLASS = "clicked";
+        title.classList.add(let_title_CLASS);
+    } else {
+        title.classList.remove(let_title_CLASS);
+        let_title_CLASS = "unclicked";
+        title.classList.add(let_title_CLASS);
+
+    }
+
+}
+
+function init() {
+    title.addEventListener("click", handleClick);
+}
+init();
